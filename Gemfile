a623263994aa65a1cf84053209463c0f60200380
Sarah Mei <sarahmei@gmail.com>
2012-10-06 14:20:32 -0700

    add new relic

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source :rubygems
 gem 'sinatra'
 gem 'wikipedia-client'
 gem 'json_pure'
+gem 'newrelic_rpm'
--- halloween_helper.rb
+++ halloween_helper.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require 'wikipedia'
 require 'json'
 
+configure :production do
+  require 'newrelic_rpm'
+end
+
 get '/' do
   erb :index
 end
@@ -14,7 +18,6 @@ post '/' do
   article_result = Wikipedia::Client.new.request('action' => 'query', 'list' => 'random', 'rnnamespace' => '0')
   article_data = JSON.parse(article_result)["query"]["random"].first
   @article_title = article_data["title"]
-  @article_url = "http://en.wikipedia.org/wiki/#{@article_title}"
   erb :result 
 end
 
